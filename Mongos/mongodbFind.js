55c76877fb67f6c8e53ca63ad501e32437c011ef
srikanth <srikanthsree50@gmail.com>
2018-05-26 15:28:21 +0530

    first commit

---
+++ Mongos/mongodbFind.js
@@ -0,0 +1,23 @@
+const {MongoClient,ObjectID } = require('mongodb');
+var obj = new ObjectID();
+console.log(obj);
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db) => {
+if(err)
+{
+  return  console.log('unable to connect mongo server...');
+}
+console.log(' connection to mongo server Successfull.....');
+// db.collection('Todos').find().toArray().then((docs) => {
+// console.log('Todos');
+// console.log(JSON.stringify(docs,undefined,2));
+// },(err) => {
+// console.log('unable to fetch todos',err);
+// });
+
+db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos count : ${count}`);
+    },(err) => {
+    console.log('unable to fetch todos',err);
+    });
+db.close();
+});
---
+++ Mongos/mongodbconnect.js
@@ -0,0 +1,15 @@
+const MongoClient = require('mongodb').MongoClient;
+
+ // Object Destruction
+//  const {MongoClient,ObjectID } = require('mongodb');
+// var obj = new ObjectID();
+// console.log(obj);
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client) => {
+if(err)
+{
+  return  console.log('unable to connect mongo server...');
+}
+console.log(' connection to mongo server Successfull.....');
+client.collection('Todos').insertOne({text:'hell',completed:'false'});
+client.close();
+});

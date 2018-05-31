735af0dce19961fe48ee6cbaa8144a37a51d9d3d
srikanth <srikanthsree50@gmail.com>
2018-05-31 15:00:19 +0530

    Add Get /Todos route

--- server/server.js
+++ server/server.js
@@ -18,6 +18,14 @@ res.status(200).send(doc);
 })
 });
 
+app.get('/todos',(req,res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(port,() => {
     console.log(`server running at ${port}`);
 })

225ba51948a70ffee7bee9a4b3117546c6ff12d3
srikanth <srikanthsree50@gmail.com>
2018-05-31 17:10:56 +0530

    Add Mongoose Queries

---
+++ Mongos/mongooseQueries.js
@@ -0,0 +1,29 @@
+const {ObjectId} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+var id = '5b0fcaa11c3bf90758b2ff39';
+
+//ObjectId.isValid property used to check id is valid or not
+
+// Todo.find({
+//     _id : id
+// }).then((todos) => {
+//     console.log('Todos ',todos);
+// });
+
+// Todo.findOne({
+//     _id : id
+// }).then((todo) => {
+//     console.log('Todo ',todo);
+// });
+
+
+Todo.findById(id).then((todo) => {
+    if(!todo){
+        console.log('Id Not found');  
+    }
+   return console.log('Todo By Id ',todo);
+}).catch((e) =>  console.log(e) );
+
+

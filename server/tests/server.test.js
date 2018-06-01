b7fcfd1b8eebc351d4aae57064f8d4c045bd0b8b
srikanth <srikanthsree50@gmail.com>
2018-06-01 13:49:08 +0530

    get/id single todo tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectId} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectId(), 
     text:'first text todo'},{
+        _id: new ObjectId(), 
     text:'second text todo'},{
+        _id: new ObjectId(), 
     text:'third text todo'
 }];
 
@@ -69,6 +73,20 @@ describe('GET /TODOS', () => {
         .expect(200)
         .expect((res) => {
 expect(res.body.todos.length).toBe(3);
+        })
+        .end(done);
+    });
+});
+
+
+describe('GET /TODOS/:id', () => {
+
+    it('should get a single todo',(done) => {
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+expect(res.body.todo.text).toBe(todos[0].text);
         })
         .end(done);
     });

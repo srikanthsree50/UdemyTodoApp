026db5d545314169d03b18d122998481b175c9a0
srikanth <srikanthsree50@gmail.com>
2018-06-05 20:54:57 +0530

    post users login generating tokens hashing

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,13 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI)
+.then(connection => {
+    console.log('Connected to MongoDB')
+})
+.catch(error => {
+  console.log(error.message)
+ });
 
 
 module.exports = {mongoose};

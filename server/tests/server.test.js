da0502915cf07bd8c4fa8530300fbd1edc8f145c
srikanth <srikanthsree50@gmail.com>
2018-06-06 22:37:16 +0530

    TESTing Delete Logout auth tokens delete

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -296,3 +296,23 @@ request(app)
 
   });
 });
+
+describe('Delete /users/me/token', () => {
+it('should remove auth token on logout',(done) => {
+request(app)
+.delete('/users/me/token')
+.set('x-auth',users[0].tokens[0].token)
+.expect(200)
+.expect.end((err,res) => {
+  if(err){
+    return done(err);
+  }
+  
+  User.findById(users[0]._id).then((user) => {
+    expect(user.tokens.length).toBe(0);
+    done();
+  }).catch((e) => done(e));
+
+});
+});
+});
